cc227a77d10306bca84f45d1aa10e79e21b2af11
Daniel <leinadpy@gmail.com>
2023-04-29 19:51:48 -0400

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module C/Users/leina/Documents/udemy/go-aws-react/src/github.com/leinadpy/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"C/Users/leina/Documents/udemy/go-aws-react/src/github.com/leinadpy/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
