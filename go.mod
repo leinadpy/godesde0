08e387274add6ecb01e0f101d0b04ba19fff8844
Daniel <leinadpy@gmail.com>
2023-04-29 20:29:34 -0400

    Siguiente commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module C/Users/leina/Documents/udemy/go-aws-react/src/github.com/leinadpy/godesde0
+module github.com/leinadpy/godesde0
 
 go 1.20
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"C/Users/leina/Documents/udemy/go-aws-react/src/github.com/leinadpy/godesde0/variables"
+	"fmt"
+
+	"github.com/leinadpy/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1588)
+
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
